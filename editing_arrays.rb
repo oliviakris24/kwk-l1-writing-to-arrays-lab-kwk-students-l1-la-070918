53afe024f3190b06d27a0606adfcc6946648dd31
oliviakris24 <okfishy@gmail.com>
2018-07-11 18:24:13 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,23 +1,22 @@
-rainbow_colors = [
+RAINBOW_COLORS = [
   "yellow",
   "default",
   "light_cyan"
 ]
 
 def change_rainbow_colors
-  rainbow_colors = []
-  rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light_red"
-  rainbow_colors[2] = "light_yellow"
-puts change_rainbow_colors
+  RAINBOW_COLORS = []
+  RAINBOW_COLORS[0] = "red"
+  RAINBOW_COLORS[1] = "light_red"
+  RAINBOW_COLORS[2] = "light_yellow"
+  return RAINBOW_COLORS
 end
 
 change_rainbow_colors
 
 
 def add_colors
-  rainbow_colors.push = "green"
-  rainbow_colors.push = "blue"
-  puts rainbow_colors
+  RAINBOW_COLORS.push = "green"
+  RAINBOW_COLORS.push = "blue"
 end
 
